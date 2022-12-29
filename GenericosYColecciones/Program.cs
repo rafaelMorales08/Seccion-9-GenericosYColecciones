a3f11634ce06c77400f04d277f4a0eafea316a12
rmorales <rmorales@MTY-LP-RMORALES>
2022-12-29 17:09:20 -0600

    Agregar archivos de proyecto.

---
+++ GenericosYColecciones/Program.cs
@@ -0,0 +1,45 @@
+﻿//ayuda ahorarr codigo
+
+int[] enteros = { 1, 2, 3 };
+string[] nombres = { "Felipe", "Claudia", "Pedro", "Juana" };
+
+DateTime[] fechas = { DateTime.Today, DateTime.Today.AddDays(1) };
+
+
+Console.WriteLine($"Logituf de arreglos de entero{calcularLongitudArreglo<int>(enteros)}");
+
+int calcularLongitudArreglo<T>(T[] arreglo)
+{
+    return arreglo.Length;
+}
+
+T? primerElementoOvalorPorDefecto<T>(T[] arreglo) 
+{
+    if(arreglo.Length == 0)
+    {
+        return default;
+    }
+    return arreglo[0];
+
+}
+
+Console.WriteLine(primerElementoOvalorPorDefecto(nombres));
+
+//listas
+
+int[] enteros2 = {1,2, 3 };
+
+//enteros2.Add(4);
+
+List<int> enterosLista = new List<int>();
+enterosLista.Add(1);
+enterosLista.Add(2);
+
+
+foreach (var item in enterosLista)
+{
+    Console.WriteLine(item);
+}
+  
+
+
